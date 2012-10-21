87f28c905560e1bbe079be598a9e3e503894c926
arturopst <arturo.soto@outlook.com>
2012-10-20 23:17:39 -0600

    Project Created

---
+++ TacoTraining.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TacoTraining.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TacoTraining.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TacoTraining.Web.WebApiApplication" Language="C#" %>
